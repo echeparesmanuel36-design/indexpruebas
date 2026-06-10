4d09492933749af0b5e2b7303c76a6a187506575
Manuel Echepares | AXIOM Systems ◤A-S◢ <echeparesmanuel36@gmail.com>
2026-06-10 15:17:34 +0200

    Update sw.js

--- web/sw.js
+++ web/sw.js
@@ -1,44 +1,22 @@
 const CACHE_NAME = "axiom-qr-factory-v1";
 const urlsToCache = [
-    "./",
-    "./index.html",
-    "./manifest.json",
-    "./css/styles.css",
-    "./css/addons.css",
-    "./js/utils.js",
-    "./js/ui.js",
-    "./js/pwa.js",
-    "./js/share.js",
-    "./js/shortcuts.js",
-    "./js/voice.js",
-    "./wasm/motor.js",
-    "./wasm/motor.wasm",
-    "./assets/favicon.png",
-    "./apple-touch-icon.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./css/styles.css",
+  "./css/addons.css",
+  "./js/ui.js",
+  "./assets/favicon.png"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-            );
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
